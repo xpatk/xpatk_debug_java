f2ef3c6294846ed08a276f92b3c54ae8f51ed38d
xpat <patrycja.kruczynska@gmail.com>
2025-01-02 17:38:06 +0100

    writer and reader introduced in the main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,17 +13,19 @@ import java.util.Map;
  */
 
 public class AnalyticsCounter {
-	
+
 	public static void main(String[] args) {
 
-		}} catch (IOException e) {
-			System.err.println("Error reading the file");
+		ISymptomReader symptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		List<String> symptoms = symptomReader.GetSymptoms();
+
+		Map<String, Integer> symptomCounts = new HashMap<>();
+		for (String symptom : symptoms) {
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
 		}
 
-		try (FileWriter writer = new FileWriter ("result.out")) {
+		ISymptomWriter symptomWriter = new WriteSymptomDataToFile("result.out");
+		symptomWriter.writeSymptoms(symptomCounts);
 
-		} catch (IOException e) {
-			System.err.println("Cannot write in the file.");
-		}
 	}
 }
