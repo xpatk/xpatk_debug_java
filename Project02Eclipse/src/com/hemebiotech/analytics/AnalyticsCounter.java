02d220f4112625d593820753ddfc50bad3578343
xpat <patrycja.kruczynska@gmail.com>
2025-01-17 17:34:54 +0100

    refactored AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * The main class that reads symptoms from a given file and counts the occurrence of the symptoms
@@ -14,18 +15,78 @@ import java.util.Map;
 
 public class AnalyticsCounter {
 
+	private final ISymptomReader reader;
+	private final ISymptomWriter writer;
+
+	/**
+	 * Constructor for AnalyticsCounter
+	 *
+	 * @param reader The symptom reader implementation
+	 * @param writer The symptom writer implementation
+	 */
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+	/**
+	 * Gets the list of symptoms from the input source
+	 *
+	 * @return List of symptoms
+	 */
+	public List<String> getSymptoms() {
+		return reader.GetSymptoms();
+	}
+
+	/**
+	 * Counts symptoms
+	 *
+	 * @param symptoms List of symptoms to count
+	 * @return Map of symptoms and their occurrence
+	 */
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> symptomCounts = new HashMap<>();
+		for (String symptom : symptoms) {
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
+		}
+		return symptomCounts;
+	}
+
+
+	/**
+	 * Sorts the symptoms alphabetically
+	 *
+	 * @param symptoms Map of symptoms and their counts to sort
+	 * @return Sorted map of symptoms
+	 */
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		Map<String, Integer> sortedSymptoms = new TreeMap<>();
+		sortedSymptoms.putAll(symptoms);
+		return sortedSymptoms;
+	}
+
+	/**
+	 * Writes the symptoms and their counts to the output destination
+	 *
+	 * @param symptoms Map of symptoms and their counts to write
+	 */
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.writeSymptoms(symptoms);
+	}
+
 	public static void main(String[] args) {
 		try {
 			ISymptomReader symptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
-			List<String> symptoms = symptomReader.GetSymptoms();
-
-			Map<String, Integer> symptomCounts = new HashMap<>();
-			for (String symptom : symptoms) {
-				symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
-			}
-
 			ISymptomWriter symptomWriter = new WriteSymptomDataToFile("result.out");
-			symptomWriter.writeSymptoms(symptomCounts);
+
+			AnalyticsCounter counter = new AnalyticsCounter(symptomReader, symptomWriter);
+
+			List<String> symptoms = counter.getSymptoms();
+			Map<String, Integer> symptomCounts = counter.countSymptoms(symptoms);
+			Map<String, Integer> sortedSymptoms = counter.sortSymptoms(symptomCounts);
+			counter.writeSymptoms(sortedSymptoms);
+
 		} catch (Exception e) {
 			System.err.println("An error occurred while processing files: " + e.getMessage());
 			e.printStackTrace();
